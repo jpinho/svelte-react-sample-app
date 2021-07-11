3ba838330cbe7cd8e6df680077fc0a7b567b2316
João Pinho <j.pinho@epilot.cloud>
2021-07-11 01:29:51 +0100

    chore: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -40,7 +40,8 @@ export default {
 	},
 	plugins: [
 		replace({
-      'process.env.NODE_ENV': JSON.stringify( 'development' )
+      'process.env.NODE_ENV': JSON.stringify( 'development' ),
+			preventAssignment: true
     }),
 		svelte({
 			compilerOptions: {
